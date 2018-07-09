8dc7766aa18b82be7a8787865ae88501c39ca5c8
KP <krzysztof.pankowski@oracle.com>
2018-07-09 16:03:22 +0200

    refactoring

--- scripts/BME280.py
+++ scripts/BME280.py
@@ -1,31 +1,31 @@
 from datetime import datetime
-import time
+from apscheduler.schedulers.blocking import BlockingScheduler
+from FileUtil import FileUtil
+from LoggerUtil import LoggerUtil
 import ConfigParser
-import os
-import json
-import urllib
-import urllib2
 from Adafruit_BME280 import *
 
 # read properties
 properties = ConfigParser.ConfigParser()
 properties.read('/etc/antReader.cfg')
 
-bme280_devId = properties.get('general', 'deviceId') 
+logDir = properties.get('general', 'logDir')
+
+bme280_devId = properties.get('general', 'deviceId')
 bme280_name = properties.get('bme280', 'name') 
 bme280_interval = int(properties.get('bme280', 'interval')) 
 bme280_reportDir = properties.get('general', 'reportDir')
 
+#set Scheduler
+scheduler = BlockingScheduler()
+
+#Set Logger
+logger = LoggerUtil(logDir,bme280_name)
+
 # Create new sensor instance
 sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
 
-timePrev = 0
-while True:
-  timeNow = int(time.time())
-  dateNow = time.strftime('%Y-%m-%dT%H:%M:%S.000000000%z', time.gmtime())
-  
-  if timeNow >= timePrev + bme280_interval:
-       
+def getBME280Reading():     
       # grab data from sensor     
       sensorValues = [sensor.read_temperature(), sensor.read_humidity(), sensor.read_pressure() / 100]
 
@@ -34,13 +34,8 @@ while True:
       
       #print sensorReportLine
    
-      tmpFile= "%s/%s.tmp" % (bme280_reportDir,  bme280_name)
-      fs = open(tmpFile, "w") 
-      #fs.write(sensorReportLine)
-      fs.write(sensorValue)
-      fs.close()
+      FileUtil.saveToNewFile(repobme280_reportDir,bme280_name,sensorValue)
+      logger.log(sensorValue)
       
-      dataFile= "%s/%s" % (bme280_reportDir,  bme280_name)
-      os.rename(tmpFile, dataFile) 
-      timePrev = timeNow
-      time.sleep(bme280_interval)
+scheduler.add_job(getBME280Reading, 'interval', seconds=bme280_interval)
+scheduler.start()
--- scripts/LSM9DS0.py
+++ scripts/LSM9DS0.py
@@ -1,11 +1,10 @@
 from datetime import datetime
-import time
+from apscheduler.schedulers.blocking import BlockingScheduler
+from FileUtil import FileUtil
+from LoggerUtil import LoggerUtil
 import ConfigParser
-import os
-import json
-import urllib
-import urllib2
 import Adafruit_LSM9DS0
+
 
 # read properties
 properties = ConfigParser.ConfigParser()
@@ -22,37 +21,31 @@ logFile = "%s%s.log" % (logDir, lsm9ds0_name)
 # Create new sensor instance
 imu = Adafruit_LSM9DS0.LSM9DS0()
 
-logFile = open(logFile, 'a')
+#set Scheduler
+scheduler = BlockingScheduler()
 
-timePrev = 0
-while True:
-  timeNow = int(time.time())
-  dateNow = time.strftime('%Y-%m-%dT%H:%M:%S.000000000%z', time.gmtime())
-  logDate = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime())
-   
-  if timeNow >= timePrev + lsm9ds0_interval:
+#Set Logger
+logger = LoggerUtil(logDir,lsm9ds0_name)
+
+def getLSM9DS0Reading():
       # grab data from sensor 
       gyro, mag, accel = imu.read()
        
       sensorValues = [gyro, mag, accel]
+      print sensorValues
       
       gyro_x, gyro_y, gyro_z = gyro
       mag_x, mag_y, mag_z = mag
       accel_x, accel_y, accel_z = accel
       
       sensorValue = "%s; %s; %s; %s; %s; %s; %s; %s; %s" % (gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z, accel_x, accel_y, accel_z)
-      logFile.write("%s, Gyro: %s %s %s, Mag: %s %s %s, Accel: %s %s %s\n" % (logDate, gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z, accel_x, accel_y, accel_z))
+      logger.log("%s, Gyro: %s %s %s, Mag: %s %s %s, Accel: %s %s %s\n" % (logDate, gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z, accel_x, accel_y, accel_z))
       print sensorValue
    
-      tmpFile= "%s/%s.tmp" % (lsm9ds0_reportDir,  lsm9ds0_name)
-      fs = open(tmpFile, "w") 
-      fs.write(sensorValue)
-      fs.close()
-       
-      dataFile= "%s/%s" % (lsm9ds0_reportDir,  lsm9ds0_name)
-      os.rename(tmpFile, dataFile) 
-      timePrev = timeNow
-      time.sleep(lsm9ds0_interval)
-      logFile.close()
+      FileUtil.saveToNewFile(lsm9ds0_reportDir, lsm9ds0_name,sensorValue)
+      
+
+scheduler.add_job(getLSM9DS0Reading, 'interval', seconds=lsm9ds0_interval)
+scheduler.start()
        
 
