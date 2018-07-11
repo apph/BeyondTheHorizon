335a578219bbbb011c5245f57faaa68c528cb840
KP <krzysztof.pankowski@oracle.com>
2018-07-11 14:16:38 +0200

    fixes

--- scripts/modemRock.py
+++ scripts/modemRock.py
@@ -14,6 +14,7 @@ GPS_MULTIPLIER = 1000000
 PRESSURE_MULTIPLIER = 10
 TEMPERATURE_MULTIPLIER = 100
 
+RFID_READ_FAILURE = 111
 
 # read properties
 properties = ConfigParser.ConfigParser()
@@ -28,7 +29,6 @@ maxRockMessages = properties.get('rockBLOCK', 'maxMessages')
 rockPollInterval = int(properties.get('rockBLOCK', 'pollInterval'))
 minInterval = int(properties.get('rockBLOCK', 'minInterval'))
 maxInterval = int(properties.get('rockBLOCK', 'maxInterval'))
-
 sensorsList = properties.get('rockBLOCK', 'sensors')
 logDir = properties.get('general', 'logDir')
 
@@ -44,9 +44,7 @@ rfidFile = "%s%s" % (reportDir, rfid_name)
 #set Scheduler
 #scheduler = BlockingScheduler()
 
-#TODO send message on start?
-
-class RockBlockWrapper(rockBlockProtocol):
+#TODO send message on start?class RockBlockWrapper(rockBlockProtocol):
     logger = LoggerUtil(logDir, modem_name)
     messagesCount = 0
 
@@ -97,129 +95,133 @@ class RockBlockWrapper(rockBlockProtocol):
         forceSendData = False
 
         while True:
-            try:
-                timeNow = int(time.time())
-                dateNow = time.strftime('%Y%m%dT%H%M%S', time.gmtime())
-                # init to default values if nothing comes from the sensor files
-                latConv = self.convertToHex(0.0, GPS_MULTIPLIER, 8)
-                lonConv = self.convertToHex(0.0, GPS_MULTIPLIER, 8)
-                airTempConv = self.convertToHex(0.0, TEMPERATURE_MULTIPLIER, 4)
-                humidityConv = self.convertToHex(0, 1, 2)
-                pressureConv = self.convertToHex(0.0, PRESSURE_MULTIPLIER, 4)
-                waterTempConv = self.convertToHex(0.0, TEMPERATURE_MULTIPLIER, 4)
-                lightConv = self.convertToHex(0, 1, 6)
-                rfidConv = self.convertToHex(0, 1, 2)
-                
-                # pre-initialize array with 8 elements
-                sensorRawData = [None] * 8
+            timeNow = int(time.time())
+            dateNow = time.strftime('%Y%m%dT%H%M%S', time.gmtime())
+            # init to default values if nothing comes from the sensor files
+            latConv = self.convertToHex(0.0, GPS_MULTIPLIER, 8)
+            lonConv = self.convertToHex(0.0, GPS_MULTIPLIER, 8)
+            airTempConv = self.convertToHex(0.0, TEMPERATURE_MULTIPLIER, 4)
+            humidityConv = self.convertToHex(0, 1, 2)
+            pressureConv = self.convertToHex(0.0, PRESSURE_MULTIPLIER, 4)
+            waterTempConv = self.convertToHex(0.0, TEMPERATURE_MULTIPLIER, 4)
+            lightConv = self.convertToHex(0, 1, 6)
+            # no swimmer? -> FF
+            rfidConv = self.convertToHex(255, 1, 2)
+            
+            # pre-initialize array with 8 elements
+            sensorRawData = [None] * 8
 
-                # check if there was RFID file modification. If yes, it means swimmer has changed
+            # check if there was RFID file modification. If yes, it means swimmer has changed
+            rfidMTime = 0
+            if os.path.isfile(rfidFile):
                 rfidMTime = int(os.stat(rfidFile).st_mtime)
-                
-                # if we have reached maximum number of messages stop sending (will be too expensive)
-                if sentRockMessages > maxRockMessages:
-                    break
-              
-                # change of swimmer, send data
-                if rfidMTime >= timePrev:
-                    forceSendData = True
+            
+            # if we have reached maximum number of messages stop sending (will be too expensive)
+            if sentRockMessages > maxRockMessages:
+                break
+          
+            # change of swimmer, send data
+            if rfidMTime >= timePrev:
+                forceSendData = True
 
-                # do we need to poll RockBLOCK for configuration updates
-                if (timeNow >= timePrev + rockPollInterval):
-                    if self.messagesCount > 0:
-                        print "Invoke RequestMessages"
-                        try:
-                            self.requestMessages(rockBLOCKDevice)
-                        except rockBlockException as e:
-                            print "self.requestMessages(rockBlockException)"
-                            self.logger.log("RockBLOCK - requestMessages, exception: %s" % str(e))
-
-                if (timeNow >= timePrev + modem_interval) or forceSendData:
-                    forceSendData = False
-                    sensors = sensorsList.split(';')
-                    timePrev = timeNow
-
-                    #print "Sensors: %s" % sensors
-                    for cnt in range(0, len(sensors)):
-                        try: 
-                            sensor = sensors[cnt]
-                            sensorDataFile = "%s%s" % (reportDir, sensor)
-                            fs = open(sensorDataFile, "r")
-                            sensorValues = fs.read()
-
-                            if sensor == gps_name:
-                                #print "GPSData %s" % sensorValues
-                                gpsData = sensorValues.split(';')
-                                latitude = float(gpsData[0])
-                                longitude = float(gpsData[1])
-                                latConv = self.convertToHex(latitude, GPS_MULTIPLIER, 8)
-                                lonConv = self.convertToHex(longitude, GPS_MULTIPLIER, 8)
-                                sensorRawData[0] = latitude
-                                sensorRawData[1] = longitude
-                            elif sensor == bme280_name:
-                                #print "BME280Data: %s" % sensorValues
-                                bme280Data = sensorValues.split(';')
-                                airTemperature = float(bme280Data[0])
-                                humidity = float(bme280Data[1])
-                                pressure = float(bme280Data[2])
-                                airTempConv = self.convertToHex(airTemperature, TEMPERATURE_MULTIPLIER, 4)
-                                humidityConv = self.convertToHex(humidity, 1, 2)
-                                pressureConv = self.convertToHex(pressure, PRESSURE_MULTIPLIER, 4)
-                                sensorRawData[2] = airTemperature
-                                sensorRawData[3] = humidity
-                                sensorRawData[4] = pressure
-                            elif sensor == light_name:
-                                #print "LightData: %s" % sensorValues
-                                light = sensorValues
-                                lightConv = self.convertToHex(light, 1, 6)
-                                sensorRawData[5] = light
-                            elif sensor == rfid_name:
-                                #print "RFIDData: %s" % sensorValues
-                                rfid = sensorValues
-                                rfidConv = self.convertToHex(rfid, 1, 2)
-                                sensorRawData[6] = rfid
-                            elif sensor == water_name:
-                                #print "WaterData: %s" % sensorValues
-                                water = float(sensorValues)
-                                waterTempConv = self.convertToHex(water, TEMPERATURE_MULTIPLIER, 4)
-                                sensorRawData[7] = water
-                            else:
-                                # unknown sensor
-                                self.logger.log("%s - unknown sensor: %s" % (dateNow, sensor))
-                                pass
-
-                            fs.close()    
-                            
-                        except IOError as (errno, strerror):
-                            self.logger.log("I/O error({0}): {1}".format(errno, strerror))
-                        except Exception as e:
-                            self.logger.log("Unexpected error: {0}".format(e))
-                            
-                    deviceIdHex = self.convertToHex(deviceId, 1, 2)
-                    timeNow = int(time.time())
-                    dateHex = self.convertToHex(timeNow, 1, 8)
-                    # unknown sensor value
-                    print "Raw sensor data: %s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (sensorRawData[0], sensorRawData[1], timeNow, sensorRawData[5], sensorRawData[2],
-                                                                            sensorRawData[3], sensorRawData[4], sensorRawData[7], deviceId, sensorRawData[6])
-                    self.logger.log("Raw sensor data: %s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n" % (sensorRawData[0], sensorRawData[1], timeNow, sensorRawData[5], sensorRawData[2],
-                                                                            sensorRawData[3], sensorRawData[4], sensorRawData[7], deviceId, sensorRawData[6]))
-                    # assemble data
-                    
-                    print "%s%s%s%s%s%s%s%s%s%s" % (latConv, lonConv, dateHex, lightConv, airTempConv, humidityConv, pressureConv, waterTempConv, deviceId, rfidConv)
-                    assembledDataHex = "%s%s%s%s%s%s%s%s%s%s" % (latConv, lonConv, dateHex, lightConv, airTempConv, humidityConv, pressureConv, waterTempConv, deviceIdHex, rfidConv)
-                    self.logger.log("Hex data: %s" % assembledDataHex)
-                    
-                    # send data
+            # do we need to poll RockBLOCK for configuration updates
+            if (timeNow >= timePrev + rockPollInterval):
+                if self.messagesCount > 0:
+                    print "Invoke RequestMessages"
                     try:
-                        self.sendMessage(assembledDataHex, rockBLOCKDevice)
-                        sentRockMessages = sentRockMessages + 1
-                        self.logger.log("Sent message number: %s" % sentRockMessages)
+                        self.requestMessages(rockBLOCKDevice)
                     except rockBlockException as e:
-                        print "rockBlockException"
-                        self.logger.log("RockBLOCK Exception: %s" % str(e))
+                        print "self.requestMessages(rockBlockException)"
+                        self.logger.log("RockBLOCK - requestMessages, exception: %s" % str(e))
+
+            if (timeNow >= timePrev + modem_interval) or forceSendData:
+                forceSendData = False
+                sensors = sensorsList.split(';')
+                timePrev = timeNow
+
+                #print "Sensors: %s" % sensors
+                for cnt in range(0, len(sensors)):
+                    try: 
+                        sensor = sensors[cnt]
+                        sensorDataFile = "%s%s" % (reportDir, sensor)
+                        fs = open(sensorDataFile, "r")
+                        sensorValues = fs.read()
+
+                        if sensor == gps_name:
+                            #print "GPSData %s" % sensorValues
+                            gpsData = sensorValues.split(';')
+                            latitude = float(gpsData[0])
+                            longitude = float(gpsData[1])
+                            latConv = self.convertToHex(latitude, GPS_MULTIPLIER, 8)
+                            lonConv = self.convertToHex(longitude, GPS_MULTIPLIER, 8)
+                            sensorRawData[0] = latitude
+                            sensorRawData[1] = longitude
+                        elif sensor == bme280_name:
+                            #print "BME280Data: %s" % sensorValues
+                            bme280Data = sensorValues.split(';')
+                            airTemperature = float(bme280Data[0])
+                            humidity = float(bme280Data[1])
+                            pressure = float(bme280Data[2])
+                            airTempConv = self.convertToHex(airTemperature, TEMPERATURE_MULTIPLIER, 4)
+                            humidityConv = self.convertToHex(humidity, 1, 2)
+                            pressureConv = self.convertToHex(pressure, PRESSURE_MULTIPLIER, 4)
+                            sensorRawData[2] = airTemperature
+                            sensorRawData[3] = humidity
+                            sensorRawData[4] = pressure
+                        elif sensor == light_name:
+                            #print "LightData: %s" % sensorValues
+                            light = sensorValues
+                            lightConv = self.convertToHex(light, 1, 6)
+                            sensorRawData[5] = light
+                        elif sensor == rfid_name:
+                            #print "RFIDData: %s" % sensorValues
+                            rfid = sensorValues
+                            if rfid == RFID_READ_FAILURE:
+                                rfidConv = self.convertToHex(255, 1, 2)
+                            else:    
+                                rfidConv = self.convertToHex(rfid, 1, 2)
+                            sensorRawData[6] = rfid
+                        elif sensor == water_name:
+                            #print "WaterData: %s" % sensorValues
+                            water = float(sensorValues)
+                            waterTempConv = self.convertToHex(water, TEMPERATURE_MULTIPLIER, 4)
+                            sensorRawData[7] = water
+                        else:
+                            # unknown sensor
+                            self.logger.log("%s - unknown sensor: %s" % (dateNow, sensor))
+                            pass
+
+                        fs.close()    
+                        
+                    except IOError as (errno, strerror):
+                        self.logger.log("I/O error({0}): {1}".format(errno, strerror))
+                    except Exception as e:
+                        self.logger.log("Unexpected error: {0}".format(e))
+                        
+                deviceIdHex = self.convertToHex(deviceId, 1, 2)
+                timeNow = int(time.time())
+                dateHex = self.convertToHex(timeNow, 1, 8)
+                # unknown sensor value
+                print "Raw sensor data: %s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (sensorRawData[0], sensorRawData[1], timeNow, sensorRawData[5], sensorRawData[2],
+                                                                        sensorRawData[3], sensorRawData[4], sensorRawData[7], deviceId, sensorRawData[6])
+                self.logger.log("Raw sensor data: %s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n" % (sensorRawData[0], sensorRawData[1], timeNow, sensorRawData[5], sensorRawData[2],
+                                                                        sensorRawData[3], sensorRawData[4], sensorRawData[7], deviceId, sensorRawData[6]))
+                # assemble data
                 
-                except Exception as e:
-                    self.logger.log("General exception: %s" % str(e))
+                print "%s%s%s%s%s%s%s%s%s%s" % (latConv, lonConv, dateHex, lightConv, airTempConv, humidityConv, pressureConv, waterTempConv, deviceId, rfidConv)
+                assembledDataHex = "%s%s%s%s%s%s%s%s%s%s" % (latConv, lonConv, dateHex, lightConv, airTempConv, humidityConv, pressureConv, waterTempConv, deviceIdHex, rfidConv)
+                self.logger.log("Hex data: %s" % assembledDataHex)
+                
+                # send data
+                try:
+                    #rbs = RockBLOCKSend()
+                    print "Device: %s" % rockBLOCKDevice
+                    self.sendMessage(assembledDataHex, rockBLOCKDevice)
+                    sentRockMessages = sentRockMessages + 1
+                    self.logger.log("Sent message number: %s" % sentRockMessages)
+                except rockBlockException as e:
+                    print "rockBlockException %s " % e
+                    self.logger.log("RockBLOCK Exception: %s" % str(e))
 
 
 rbw = RockBlockWrapper()
